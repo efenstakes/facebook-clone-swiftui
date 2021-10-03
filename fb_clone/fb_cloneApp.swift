4799182445fa4915009d0d7fc60d486922750696
kipchumba26 <cynthia.kipchumba@nativecoders.se>
2021-10-03 19:50:47 +0300

    Initial Commit

---
+++ fb_clone/fb_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  fb_cloneApp.swift
+//  fb_clone
+//
+//  Created by MAC on 10/3/21.
+//
+
+import SwiftUI
+
+@main
+struct fb_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
